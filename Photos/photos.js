8c94cf993ec2c3a44c7d3eb5e7efcb04df35317d
Beowulf <92696505+Beowulf1Nly@users.noreply.github.com>
2026-04-14 16:21:51 -0400

    Delete Photos/photos.js

--- Photos/photos.js
+++
@@ -1,8 +0,0 @@
-// ─────────────────────────────────────────────
-// Add or remove photos here.
-// Just add the filename — keep all photos in this same folder.
-// ─────────────────────────────────────────────
-const PHOTOS = [
-  "Photos/image1.webp",
-  "Photos/image2.webp"
-];
